2b7c73e7be6158b31aaa4f3cbc36c2bd443aba84
remote-glitch-3651 <moniquedantoine17@gmail.com>
2020-08-30 21:57:49 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,21 +1,15 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-
-  
-def parrot (name = "Pretty bird!")
+def parrot (name = "Pretty bird!", parrot)
   puts "Pretty bird!"
   parrot = "Pretty bird!"
 end
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
-
-
   
-def parrot (name = "Pretty bird!")
-  puts "Pretty bird!"
-  parrot = "Pretty bird!"
-end
+def parrot 
+  puts "Squawk!"
+  parrot = "Squawk!"
+
 
 
 
