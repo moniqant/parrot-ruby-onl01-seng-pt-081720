cb21bedb57260b987cbcb4b91ece9740721466a6
remote-glitch-3651 <moniquedantoine17@gmail.com>
2020-08-30 20:57:30 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  restaurant_name = "Guy's American Kitchen & Bar"
+  cuisine = "american"
+  motto = "Welcome to Flavor Town!"
+end
